116344b9ac894ff417c8d3d01287d9288dd06c3a
Christiaan <chis.wol@gmail.com>
2023-01-07 12:45:26 +0200

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage marks did you get on your exam? ");
+        string value = Console.ReadLine();
+
+        int i = int.Parse(value);
+
+        if(i >= 90) 
+        {
+            Console.WriteLine("You have a grade average of A.");
+        }
+        else if(i < 90 && i >= 80) 
+        {
+            Console.WriteLine("You have a grade average of B.");
+        }
+        else if(i < 80 && i >= 70) 
+        {
+            Console.WriteLine("You have a grade average of C.");
+        }
+        else if(i < 70 && i >= 60) {
+            Console.WriteLine("You have a grade average of D.");
+        }
+        else
+        {
+            Console.WriteLine("You have a grade average of F.");
+        }
+        
     }
 }
