73166ca40451395e820a38748b5c9aef81b91535
Christiaan <chis.wol@gmail.com>
2023-01-09 17:56:19 +0200

    changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string input = "yes";
+
+        Console.Write("Do you wish to play a game? ");
+        input = Console.ReadLine();
+
+        while (input == "yes")
+        {
+            Console.Write("What is the magic number? ");
+            string num_input = Console.ReadLine();
+            Console.WriteLine($"You guessed {num_input}");
+
+            int number = int.Parse(num_input);
+
+            if (number == 7) 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (number > 7)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else 
+            {
+                Console.WriteLine("Guess Higher");
+            }
+        }
+
     }
 }
